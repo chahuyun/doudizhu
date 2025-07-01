09df419ad1e98b40707679530a19908bf45c641c
Moyuyanli <572490972@qq.com>
2025-07-01 15:27:16 +0800

    refactor(MessageUtil): 重构 MessageUtil 类
    - 将 MessageUtil 类改为单例对象
    -优化 nextMessage 方法，移除可空返回值
    - 使用 first替代 firstOrNull，确保始终返回结果

--- src/main/kotlin/cn/chahuyun/doudizhu/MessageUtil.kt
+++ src/main/kotlin/cn/chahuyun/doudizhu/MessageUtil.kt
@@ -1,25 +1,21 @@
 package cn.chahuyun.doudizhu
 
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
 import net.mamoe.mirai.contact.Contact
 import net.mamoe.mirai.event.events.MessageEvent
 
-class MessageUtil {
+object MessageUtil {
 
 
-    companion object {
-
-        suspend fun nextMessage(sender: Contact): MessageEvent? {
-            return nextMessage(sender.id)
-        }
-
-        suspend fun nextMessage(senderId: Long): MessageEvent? = callbackFlow {
-             DouDiZhu.channel.filter { it.sender.id == senderId }.subscribeOnce<MessageEvent> { trySend(it) }
-        }.firstOrNull()
-
-
+    suspend fun nextMessage(sender: Contact): MessageEvent {
+        return nextMessage(sender.id)
     }
+
+    suspend fun nextMessage(senderId: Long): MessageEvent = callbackFlow {
+        DouDiZhu.channel.filter { it.sender.id == senderId }.subscribeOnce<MessageEvent> { trySend(it) }
+    }.first()
 
 
 }
