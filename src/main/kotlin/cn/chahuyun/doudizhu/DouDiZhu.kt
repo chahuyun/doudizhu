a262243f597715bccc2788ce74897c02adf4be83
Moyuyanli <572490972@qq.com>
2025-07-03 17:45:58 +0800

    feat(PlayerUtil): 添加玩家好友工具类
    - 新增 PlayerUtil 类，用于处理玩家好友相关操作- 实现了检查玩家是否为好友、监听新好友请求等功能- 优化了 DouDiZhu 类，添加了 bot 和 scope属性
    - 在 onEnable 中添加了 BotOnlineEvent 监听，用于初始化 bot

--- src/main/kotlin/cn/chahuyun/doudizhu/DouDiZhu.kt
+++ src/main/kotlin/cn/chahuyun/doudizhu/DouDiZhu.kt
@@ -1,10 +1,14 @@
 package cn.chahuyun.doudizhu
 
 import cn.chahuyun.authorize.PermissionServer
+import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.nameCardOrNick
+import net.mamoe.mirai.event.Event
 import net.mamoe.mirai.event.EventChannel
 import net.mamoe.mirai.event.GlobalEventChannel
+import net.mamoe.mirai.event.events.BotOnlineEvent
 import net.mamoe.mirai.event.events.MessageEvent
 
 object DouDiZhu : KotlinPlugin(
@@ -15,16 +19,33 @@ object DouDiZhu : KotlinPlugin(
         info("FeaFox-DouDiZhu")
     }
 ) {
+    /**
+     * 全局bot,默认第一个登录的bot
+     */
+    lateinit var bot: Bot
 
+    /**
+     * 全局事件通道
+     */
+    lateinit var scope: EventChannel<Event>
+
+    /**
+     * 全局息事件通道
+     */
     lateinit var channel: EventChannel<MessageEvent>
     override fun onEnable() {
         DZConfig.reload()
 
         PermissionServer.registerMessageEvent(this, "cn.chahuyun.doudizhu")
 
-        val scope = GlobalEventChannel.parentScope(this)
-        channel = scope.filterIsInstance(MessageEvent::class)
 
+        val scope = GlobalEventChannel.parentScope(this)
+        this@DouDiZhu.scope = scope
+        channel = scope.filterIsInstance(MessageEvent::class)
+        scope.filterIsInstance(BotOnlineEvent::class).subscribeOnce<BotOnlineEvent> {
+            this@DouDiZhu.bot = it.bot
+            logger.info("斗地主bot已配置为${it.bot.nameCardOrNick}")
+        }
     }
 
 
---
+++ src/main/kotlin/cn/chahuyun/doudizhu/PlayerUtil.kt
@@ -0,0 +1,60 @@
+package cn.chahuyun.doudizhu
+
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.firstOrNull
+import net.mamoe.mirai.event.ListeningStatus
+import net.mamoe.mirai.event.events.MessageEvent
+import net.mamoe.mirai.event.events.NewFriendRequestEvent
+
+/**
+ * 好友工具
+ */
+class PlayerUtil(private val players:List<Player>) {
+
+    /**
+     * 非好友玩家
+     */
+    private  var unFriend = mutableListOf<Player>()
+
+    /**
+     * 检查所有玩家是否已经是好友，如果是则返回true。
+     */
+    fun check(): Boolean {
+        unFriend.clear() // 清空未添加好友列表
+        players.forEach {
+            if (DouDiZhu.bot.getFriend(it.id) == null) {
+                unFriend.add(it)
+            }
+        }
+        return unFriend.isEmpty()
+    }
+
+    /**
+     * 监听新好友请求事件，直到所有玩家都成为好友或超时。
+     */
+    suspend fun listening(): Boolean {
+        return callbackFlow {
+            DouDiZhu.scope.subscribe<NewFriendRequestEvent> { event ->
+                val player = unFriend.find { it.id == event.fromId }
+                if (player != null) {
+                    unFriend.remove(player)
+                    event.accept()
+                    if (unFriend.isEmpty()) {
+                        trySend(true)
+                        ListeningStatus.STOPPED
+                    } else {
+                        ListeningStatus.LISTENING
+                    }
+                } else {
+                    event.reject(false)
+                    ListeningStatus.LISTENING
+                }
+            }
+        }.firstOrNull() ?: false
+    }
+
+    /**
+     * 获取好友的下一条消息
+     */
+    suspend fun Player.nextMessage():MessageEvent? = MessageUtil.nextMessage(this,30)
+}
