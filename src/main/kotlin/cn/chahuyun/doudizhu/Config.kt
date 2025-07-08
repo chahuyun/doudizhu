505a7b53108da47939764790a9ec62e94e9c28b8
Moyuyanli <572490972@qq.com>
2025-07-09 03:11:51 +0800

    feat: 添加斗地主插件配置
    - 新增 DZConfig 对象，包含 bot 名称和创建对局等待时间的配置- 新增 DZDataConfig 对象，包含数据库类型、连接地址、用户名和密码的配置
    - 使用 AutoSavePluginConfig 实现配置的自动保存

---
+++ src/main/kotlin/cn/chahuyun/doudizhu/Config.kt
@@ -0,0 +1,32 @@
+package cn.chahuyun.doudizhu
+
+import cn.chahuyun.hibernateplus.DriveType
+import net.mamoe.mirai.console.data.AutoSavePluginConfig
+import net.mamoe.mirai.console.data.ValueDescription
+import net.mamoe.mirai.console.data.value
+
+object DZConfig : AutoSavePluginConfig("doudizhu-config") {
+
+
+    @ValueDescription("bot的代称")
+    val botName: String by value("bot")
+
+    @ValueDescription("创建对局等待时间")
+    val timeOut: Int by value(60)
+
+}
+
+object DZDataConfig: AutoSavePluginConfig("doudizhu-data") {
+
+    @ValueDescription("数据库类型(H2,MYSQL,SQLITE)")
+    var dataType: DriveType by value(DriveType.H2)
+
+    @ValueDescription("数据库连接地址")
+    var mysqlUrl : String by value("localhost:3306/test")
+
+    @ValueDescription("mysql 用户名")
+    var mysqlUser: String by value("root")
+
+    @ValueDescription("mysql 密码")
+    var mysqlPassword: String by value("123456")
+}
