0784456874d6a296b302308799e692ad81526a1e
Moyuyanli <572490972@qq.com>
2025-07-14 10:57:05 +0800

    feat(Player): 为 Player 类添加 equals 和 hashCode 方法
    - 重写 equals 方法，比较 Player 对象的 id 属性
    - 实现 hashCode 方法，使用 id 的哈希值

--- src/main/kotlin/cn/chahuyun/teafox/game/Entity.kt
+++ src/main/kotlin/cn/chahuyun/teafox/game/Entity.kt
@@ -72,6 +72,21 @@ data class Player(
     fun addCard(card: Card) {
         hand.add(card)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Player
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+
 }
 
 /**
